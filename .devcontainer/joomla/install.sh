f81c9ba47b1c24e20d59d2aa8a822a0f8b31fed3
Charley Wu <akunzai@gmail.com>
2023-07-31 19:08:25 +0800

    Set up development containers

---
+++ .devcontainer/joomla/install.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+docker exec -it magebridge-joomla test -f configuration.php
+if [ "$?" -eq "0" ]; then
+  echo "Joomla! already installed!"
+  exit 0
+fi
+
+[ -f .env ] && source .env
+
+for i in $(seq 1 20); do
+  docker exec -it magebridge-mysql sh -c "mysql -u${JOOMLA_DB_USER:-root} -p${JOOMLA_DB_PASSWORD:-${MYSQL_ROOT_PASSWORD:-secret}} -e 'show databases;' 2>/dev/null" | grep -qF 'joomla' && break
+  sleep 1
+done
+
+docker exec -it magebridge-joomla test -f installation/joomla.php
+if [ "$?" -ne "0" ]; then
+  echo "Joomla! CLI installer not found! (requires Joomla! version >= 4.3)"
+  exit 1
+fi
+
+# https://docs.joomla.org/J4.x:Joomla_CLI_Installation
+docker exec -it --user www-data magebridge-joomla php installation/joomla.php install \
+  --site-name DEMO \
+  --admin-user ADMIN  \
+  --admin-username "${ADMIN_USERNAME:-admin}" \
+  --admin-password "${ADMIN_PASSWORD:-ChangeTheP@ssw0rd}" \
+  --admin-email "${ADMIN_EMAIL:-admin@example.com}" \
+  --db-host "${JOOMLA_DB_HOST:-mysql}" \
+  --db-name "${JOOMLA_DB_NAME:-joomla}" \
+  --db-user "${JOOMLA_DB_USER:-root}" \
+  --db-pass "${JOOMLA_DB_PASSWORD:-${MYSQL_ROOT_PASSWORD:-secret}}" \
+  --db-prefix "${JOOMLA_DB_PREFIX:-vlqhe_}" \
+  --no-interaction
+
+echo "Fixing permissions ..."
+docker exec -it magebridge-joomla chown -R www-data:www-data /var/www/html
---
+++ .devcontainer/openmage/docker-entrypoint.sh
@@ -0,0 +1,64 @@
+#!/bin/env bash
+set -e
+
+if [[ "$1" == apache2* ]]; then
+    uid="$(id -u)"
+    gid="$(id -g)"
+    if [ "$uid" = '0' ]; then
+        case "$1" in
+        apache2*)
+            user="${APACHE_RUN_USER:-www-data}"
+            group="${APACHE_RUN_GROUP:-www-data}"
+
+            # strip off any '#' symbol ('#1000' is valid syntax for Apache)
+            pound='#'
+            user="${user#$pound}"
+            group="${group#$pound}"
+
+            # set user if not exist
+            if ! id "$user" &>/dev/null; then
+                # get the user name
+                : "${USER_NAME:=www-data}"
+                # change the user name
+                [[ "$USER_NAME" != "www-data" ]] &&
+                    usermod -l "$USER_NAME" www-data &&
+                    groupmod -n "$USER_NAME" www-data
+                # update the user ID
+                groupmod -o -g "$user" "$USER_NAME"
+                # update the user-group ID
+                usermod -o -u "$group" "$USER_NAME"
+            fi
+            ;;
+        *)
+            user='www-data'
+            group='www-data'
+            ;;
+        esac
+    else
+        user="$uid"
+        group="$gid"
+    fi
+    if [ ! -e '/var/www/html/app/etc/config.xml' ]; then
+        sourceTarArgs=(
+            --create
+            --file -
+            --directory /usr/src/openmage
+            --one-file-system
+            --owner "$user" --group "$group"
+        )
+        targetTarArgs=(
+            --extract
+            --file -
+            --directory /var/www/html
+        )
+        if [ "$uid" != '0' ]; then
+            # avoid "tar: .: Cannot utime: Operation not permitted" and "tar: .: Cannot change mode to rwxr-xr-x: Operation not permitted"
+            targetTarArgs+=(--no-overwrite-dir)
+        fi
+
+        tar "${sourceTarArgs[@]}" . | tar "${targetTarArgs[@]}"
+        echo >&2 "OpenMage has been successfully copied to /var/www/html"
+    fi
+fi
+
+exec "$@"
