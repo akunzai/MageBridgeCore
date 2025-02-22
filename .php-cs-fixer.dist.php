8a11145c82c00923a713fa49f6460271010d035a
Charley Wu <akunzai@gmail.com>
2025-02-22 10:57:49 +0800

    Enable parallel runner for PHP CS Fixer
    - https://cs.symfony.com/doc/usage.html

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -4,7 +4,10 @@ $finder = PhpCsFixer\Finder::create()
 	->in([__DIR__ . '/magento']);
 
 $config = new PhpCsFixer\Config();
-return $config->setRules([
-	'@PSR12' => true,
-	'@PHP81Migration' => true
-])->setFinder($finder);
+return $config
+	->setParallelConfig(PhpCsFixer\Runner\Parallel\ParallelConfigFactory::detect())
+	->setRules([
+		'@PSR12' => true,
+		'@PHP81Migration' => true
+	])
+	->setFinder($finder);
